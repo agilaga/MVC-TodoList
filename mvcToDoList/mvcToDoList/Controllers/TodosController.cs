793dfbe7e92b51242e3e7596192e7aadc47aaa90
AGIL AGHAZADA <aqil.agazade.97@gmail.com>
2022-12-27 16:34:06 +0400

    New

--- mvcToDoList/mvcToDoList/Controllers/TodosController.cs
+++ mvcToDoList/mvcToDoList/Controllers/TodosController.cs
@@ -43,7 +43,7 @@ namespace mvcToDoList.Controllers
 
         // POST: Todos/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ID,Name,DueDate,Attributes,Priority")] Todos todos)
